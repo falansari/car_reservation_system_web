35948cab13c001aeb717dc2206e7638a603a2abc
nadakooheji <nada-kooheji@hotmail.com>
2018-05-21 09:05:21 +0300

    navigation bar complete #1

---
+++ public_html/Header.php
@@ -0,0 +1,19 @@
+
+</head>
+<link rel="stylesheet" type="text/css" href="<?php $_SERVER['DOCUMENT_ROOT'].'/css/cars.css';?>">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+<body>
+    <div class="layer">
+    
+    <ul >
+        <li><a  href="index.php"><i class="fa fa-home"> Home</i></a></li>
+        <li><a href="About.php"> About Us</a></li>
+  <li><a href="#">Contact Us</a></li>
+  <li style="float:right"><a href="LoginPage.php"><span class="fas fa-sign-in-alt"> Login</span></a></li>
+  <li style="float:right"><a href="Register.php"><i class="fas fa-user-plus"> Register</i></a></li>
+  
+</ul>
+</div>
+</body>
+</html>
---
+++ public_html/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        include 'Header.php';
+        ?>
+        
+        <div id="sidebar">
+            
+            
+                <center><h1>Book Your Car Today!</h1></center> 
+<form action="index.php" method="post">
+    
+    
+     
+    <h4>Car manufacturer :</h4><select class="styled-select slate">
+  <option value="1">Select One:</option>
+  <option value="volvo">Any</option>
+  <option value="saab">SUV</option>
+  <option value="opel">Truck</option>
+  <option value="audi">Hybrid</option>
+</select>
+    
+    <h4>Car Category :</h4><select class="styled-select slate">
+  <option value="1">Select One:</option>
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="opel">Opel</option>
+  <option value="audi">Audi</option>
+</select>
+      
+    <center><input type ="submit" class ="button SubButton" value ="Search" /> 
+             
+            <input type="hidden" name="submitted" value="1" />
+     
+     </center>       
+</form>    
+          </div>      
+            
+    </body>
+</html>
