5480658d12884326fc193dc19a59665dc88de9a0
nadakooheji <nada-kooheji@hotmail.com>
2018-05-21 09:13:51 +0300

    site design complete closes #2

---
+++ public_html/About.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        include 'Header.php';
+        ?>
+    <center>
+        <div id="aboutsidebar">
+            <h1>About Us</h1>
+             Today the company has a fleet of all the Latest Model Vehicles in and it is well known for it's service, price and flexibility.
+        
+             <br><br><br>
+            The company is a fully owned Bahraini Company with no foreign franchise and it's considered amongst the largest fleet owner in the Country. Our success lies in our Customer’s total satisfaction as we extend to them an efficient and professional service together with our Pricing Structure. We operate our business round the clock, 365 days a year and 24 hours a day. 
+<br><br><br>
+            Being in a very successful business, we pride ourselves that our clientele extend from all Government and Semi-Government Bodies and Private Sectors. We also covered Airlines and Travel Agencies, both in and out of Bahrain. The Company is well-experienced in servicing all transportation requirements for every Major Events held in the Kingdom of Bahrain. We have an outstanding reputation in the market and our Company comes highly recommended. Check out our very economical and discount rental rates for various models and sized motor cars. If you are traveling on a budget from overseas our hire cars are at special prices.
+<br><br><br>
+The Company also offers great rental deals for people who has limited budgets. We have a selection of premium, new model cars that is available to clients for budget rental deals. Choose from our budget rental deals that you prefer, whether it is for a daily, weekly or monthly basis.
+<br><br><br>
+Our budget deals also includes bonus services to make your rental experience even better.
+        </div>
+      </center>       
+    </body>
+</html>
---
+++ public_html/LoginFunctions.php
@@ -0,0 +1,65 @@
+<?php
+include_once 'mysqli_connect.php';
+//include_once 'debugging.php';
+
+function absolute_url($page = 'Index.php')
+{
+    //header('Location: http:\\localhost');
+    //exit(); //terminates the script
+    
+    $url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
+    $url = rtrim($url, '/\\');
+    $url .= '/' . $page;
+    
+    return $url;
+}
+
+function checkLogin($username, $password) 
+{
+  $errors = array();
+    
+  if(empty($username))
+    $errors[] = 'You must enter User name';
+ 
+  if(empty($password))
+    $errors[] = 'You must enter a password';
+ 
+  if(empty($errors))
+  {
+      require_once 'mysqli_connect.php';
+////set up database econnection
+     
+         
+      $db = new Database();
+      $dbc = $db->getConnection();
+
+      $q = "SELECT * FROM articleusers where user_name = '$username' and user_pass= '$password'";
+
+      $r = mysqli_query($dbc, $q);
+                
+      if($r)
+      {
+          
+          if(mysqli_affected_rows($dbc) != 0)
+          {
+            $row = mysqli_fetch_array($r, MYSQLI_ASSOC);
+            return array(true, $row);
+          }
+          else
+          {
+            $errors[] = 'Passwords do not match';
+          }
+      }
+      else{
+        echo '<p class="error"> Oh dear. There was a databse error</p>';
+        echo '<p class = "error">' . mysqli_error($dbc) .'</p>';
+      }
+
+  }
+  
+  return array(false, $errors);
+      
+}
+
+
+?>
